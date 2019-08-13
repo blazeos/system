6d7b23e17b85361bf487fb13eb373a59b4402a5c
buzzy <daniele.testa@gmail.com>
2019-08-14 00:51:16 +0800

    Update build_arm.sh

--- scripts/build_arm.sh
+++ scripts/build_arm.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 #FETCH NEEDED TOOLS
-apt-get install -y gcc-8-aarch64-linux-gnu gcc-8-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
+apt-get install -y gcc gcc-5-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
 
 #CREATE DIR STRUCTURE
 rm -fr /opt/sysroot
