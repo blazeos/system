611cbcd30ccdeadf52543207c4be24ecd0802d8c
buzzy <daniele.testa@gmail.com>
2019-08-14 16:49:31 +0800

    Update build_arm.sh

--- scripts/build_arm.sh
+++ scripts/build_arm.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 #FETCH NEEDED TOOLS
-apt-get install -y gcc gcc-5-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
+apt-get install -y gcc gcc-5-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev device-tree-compiler
 
 #CREATE DIR STRUCTURE
 rm -fr /opt/sysroot
@@ -30,3 +30,17 @@ make -j$(nproc) Image
 make -j$(nproc) modules
 make dtbs
 make -j$(nproc)
+
+make INSTALL_MOD_PATH="/tmp/modules" modules_install
+rm -f /tmp/modules/lib/modules/*/{source,build}
+mkdir -p /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/modules
+cp -rv /tmp/modules/lib/modules/3.18.0-19095-g86596f58eadf/* /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/modules
+ln -s 3.18.0-19095-g86596f58eadf /opt/sysroot/Programs/kernel-aarch64/current
+ln -s /Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/modules /opt/sysroot/System/Kernel/Modules/3.18.0-19095-g86596f58eadf
+rm -rf /tmp/modules
+make INSTALL_DTBS_PATH="/opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/dtbs" dtbs_install
+
+cp /opt/system/signing/kernel.its .
+mkimage -D "-I dts -O dtb -p 2048" -f kernel.its vmlinux.uimg
+dd if=/dev/zero of=bootloader.bin bs=512 count=1
+echo "console=tty1 init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > cmdline
