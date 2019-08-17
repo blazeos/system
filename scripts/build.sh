79ff6953e8d48582f4fbfc910fd105e265e5be73
buzzy <daniele.testa@gmail.com>
2019-08-17 21:46:00 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -97,3 +97,44 @@ find /tmp/busybox/bin/* -type l -execdir ln -s /Programs/busybox/1.30.1/bin/busy
 find /tmp/busybox/sbin/* -type l -execdir ln -s /Programs/busybox/1.30.1/bin/busybox /opt/sysroot/System/Index/Binaries/{} ';'
 rm -fr /tmp/busybox
 
+#GLIBC
+cd /opt
+wget https://mirrors.dotsrc.org/gnu/glibc/glibc-2.29.tar.xz
+tar xfv glibc-2.29.tar.xz
+cd glibc-2.29
+mkdir build
+cd build
+
+../configure \
+  --host=arm-linux-gnueabihf \
+  --prefix= \
+  --includedir=/include \
+  --libexecdir=/libexec \
+  --with-__thread \
+  --with-tls \
+  --with-fp \
+  --with-headers=/opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers \
+  --without-cvs \
+  --without-gd \
+  --enable-kernel=3.18.0 \
+  --enable-stack-protector=strong \
+  --enable-shared \
+  --enable-add-ons=no \
+  --enable-obsolete-rpc \
+  --disable-profile \
+  --disable-debug \
+  --disable-sanity-checks \
+  --disable-static \
+  --disable-werror
+
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/glibc/2.29
+rm -rf /opt/sysroot/Programs/glibc/2.29/{libexec,share,var}
+ln -s 2.29 /opt/sysroot/Programs/glibc/current
+cp /opt/sysroot/Programs/glibc/2.29/etc/* /opt/sysroot/System/Settings
+rm -rf /opt/sysroot/Programs/glibc/2.29/etc
+
+link_files /System/Index/Binaries /Programs/glibc/2.29/bin
+link_files /System/Index/Includes /Programs/glibc/2.29/include
+link_files /System/Index/Libraries /Programs/glibc/2.29/lib
+link_files /System/Index/Binaries /Programs/glibc/2.29/sbin
