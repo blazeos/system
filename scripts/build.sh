0a3177b8d86cf5ce80003733a405c048e26909d3
buzzy <daniele.testa@gmail.com>
2019-08-17 21:09:10 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,7 @@ link_files () {
 
 #FETCH NEEDED TOOLS
 apt-get update
-apt-get install -y locales dialog
-apt-get install -y gcc gcc-8-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
+apt-get install -y locales dialog gcc gcc-8-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
 
 #CREATE DIR STRUCTURE
 rm -fr /opt/sysroot
@@ -52,15 +51,14 @@ if [ $(arch) == "aarch64" ]; then
   ln -s 3.18.0-19095-g86596f58eadf /opt/sysroot/Programs/kernel-aarch64/current
   ln -s /Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/modules /opt/sysroot/System/Kernel/Modules/3.18.0-19095-g86596f58eadf
   rm -rf /tmp/modules
-  #depmod -b /opt/sysroot/System/Kernel/Modules -F System.map "3.18.0-19095-g86596f58eadf"
 
   make INSTALL_DTBS_PATH="/opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/dtbs" dtbs_install
 
-  cp /opt/PowerOS/signing/kernel.its .
+  cp /opt/system/signing/kernel.its .
   mkimage -D "-I dts -O dtb -p 2048" -f kernel.its vmlinux.uimg
   dd if=/dev/zero of=bootloader.bin bs=512 count=1
   echo "console=tty1 init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > cmdline
-  vbutil_kernel --pack vmlinux.kpart --version 1 --vmlinuz vmlinux.uimg --arch aarch64 --keyblock /opt/PowerOS/signing/kernel.keyblock --signprivate /opt/PowerOS/signing/kernel_data_key.vbprivk --config cmdline --bootloader bootloader.bin
+  vbutil_kernel --pack vmlinux.kpart --version 1 --vmlinuz vmlinux.uimg --arch aarch64 --keyblock /opt/system/signing/kernel.keyblock --signprivate /opt/system/signing/kernel_data_key.vbprivk --config cmdline --bootloader bootloader.bin
   mkdir -p /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/image
   cp vmlinux.kpart /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/image
   ln -s /Programs/kernel-aarch64/current/image /opt/sysroot/System/Kernel/Image
@@ -71,7 +69,31 @@ if [ $(arch) == "aarch64" ]; then
   mkdir -p /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers
   cp -rv /tmp/headers/include/* /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers
   rm -fr /tmp/headers
-  #link headers to include dir??
-  find /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers \( -name .install -o -name ..install.cmd \) -delete  
+  find /opt/sysroot/Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers \( -name .install -o -name ..install.cmd \) -delete
+  link_files /System/Index/Includes /Programs/kernel-aarch64/3.18.0-19095-g86596f58eadf/headers  
+else
+  echo "Native build"
 fi
+
+#BUSYBOX:
+cd /opt
+wget https://busybox.net/downloads/busybox-1.30.1.tar.bz2
+tar xfv busybox-1.30.1.tar.bz2
+cd busybox-1.30.1
+cp /opt/system/config/config.busybox .config
+
+if [ $(arch) == "aarch64" ]; then
+  export ARCH=arm
+  export CROSS_COMPILE=arm-linux-gnueabihf-  
+  echo 'CONFIG_CROSS_COMPILER_PREFIX="arm-linux-gnueabihf-"' >> .config
+fi
+
+make -j$(nproc)
+make install
+mkdir -p /opt/sysroot/Programs/busybox/1.30.1/bin
+ln -s 1.30.1 /opt/sysroot/Programs/busybox/current
+cp /tmp/busybox/bin/busybox /opt/sysroot/Programs/busybox/1.30.1/bin
+find /tmp/busybox/bin/* -type l -execdir ln -s /Programs/busybox/1.30.1/bin/busybox /opt/sysroot/System/Index/Binaries/{} ';'
+find /tmp/busybox/sbin/* -type l -execdir ln -s /Programs/busybox/1.30.1/bin/busybox /opt/sysroot/System/Index/Binaries/{} ';'
+rm -fr /tmp/busybox
 
