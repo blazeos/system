3c074b9b02faa79fbd744ba41cff94a9483b8533
buzzy <daniele.testa@gmail.com>
2019-08-17 14:53:19 +0800

    Update and rename create_aarch64_env.sh to create_aarch64.sh

---
+++ scripts/create_aarch64.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#CONFIRMED WORKING ON DEBIAN 10 BUSTER
+
+#INSTALL NEEDED PACKAGES
+apt-get update
+apt-get install -y qemu qemu-user-static binfmt-support debootstrap git
+
+#CREATE CHROOT ENVIRONMENT
+cd /opt
+if [ -d "/opt/debian-aarch64" ]; then
+  git -C /opt/debian-aarch64/opt/system pull
+else
+  qemu-debootstrap --arch=arm64 --keyring /usr/share/keyrings/debian-archive-keyring.gpg \
+  --variant=buildd --exclude=debfoster buster debian-aarch64 http://ftp.debian.org/debian
+
+  git clone https://github.com/blazeos/system.git /opt/debian-aarch64/opt/system
+fi
+
+chroot /opt/debian-aarch64 "time bash /opt/system/script/build_aarch64.sh"
