2613096e186a229fcc8b9100fa57161871d3c3af
buzzy <daniele.testa@gmail.com>
2019-08-18 23:36:39 +0800

    Update build_chroot.sh

--- scripts/build_chroot.sh
+++ scripts/build_chroot.sh
@@ -11,5 +11,8 @@ blaze install pkg-config
 blaze install libnl
 blaze install iw
 blaze install ncurses
+blaze install curl
+blaze install tcl
+blaze install gettext
 blaze install git
 blaze install wpa_supplicant
