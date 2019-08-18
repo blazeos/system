4fc48767f39412663a552e21387e1d99f51bc2bb
buzzy <daniele.testa@gmail.com>
2019-08-18 15:18:50 +0800

    Update build_chroot.sh

--- scripts/build_chroot.sh
+++ scripts/build_chroot.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 blaze install m4
+blaze install bison
+blaze install flex
+blaze install pkg-config
+blaze install libnl
+blaze install iw
+blaze install zlib
+blaze install openssl
+blaze install ncurses
+blaze install git
+blaze install wpa_supplicant
