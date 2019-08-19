2392ae2c4f0a8b703e9b38a3152d50417bc0f543
buzzy <daniele.testa@gmail.com>
2019-08-19 14:39:28 +0800

    Update create_x64.sh

--- scripts/create_x64.sh
+++ scripts/create_x64.sh
@@ -8,32 +8,32 @@ apt-get install -y qemu qemu-user-static binfmt-support debootstrap git
 
 #CREATE CHROOT ENVIRONMENT
 cd /opt
-if [ -d "/opt/debian-aarch64" ]; then
-  git -C /opt/debian-aarch64/opt/system pull
+if [ -d "/opt/debian-x86_64" ]; then
+  git -C /opt/debian-x86_64/opt/system pull
 else
-  qemu-debootstrap --arch=arm64 --keyring /usr/share/keyrings/debian-archive-keyring.gpg \
-  --variant=buildd --exclude=debfoster buster debian-aarch64 http://ftp.debian.org/debian
+  qemu-debootstrap --arch=x86_64 --keyring /usr/share/keyrings/debian-archive-keyring.gpg \
+  --variant=buildd --exclude=debfoster buster debian-x86_64 http://ftp.debian.org/debian
 
-  git clone https://github.com/blazeos/system.git /opt/debian-aarch64/opt/system
+  git clone https://github.com/blazeos/system.git /opt/debian-x86_64/opt/system
 fi
 
-chroot /opt/debian-aarch64 "/opt/system/scripts/build.sh"
+chroot /opt/debian-x86_64 "/opt/system/scripts/build.sh"
 
-git clone https://github.com/blazeos/system.git /opt/debian-aarch64/opt/sysroot/Users/root/system
+git clone https://github.com/blazeos/system.git /opt/debian-x86_64/opt/sysroot/Users/root/system
 
-cd /opt/debian-aarch64/opt/sysroot
+cd /opt/debian-x86_64/opt/sysroot
 mount -t proc proc System/Kernel/Status/
 mount --rbind /sys System/Kernel/Hardware/
 mount --rbind /dev dev/
 
-chroot /opt/debian-aarch64/opt/sysroot "/Users/root/system/scripts/build_chroot.sh"
+chroot /opt/debian-x86_64/opt/sysroot "/Users/root/system/scripts/build_chroot.sh"
 
-cd /opt/debian-aarch64/opt/sysroot
+cd /opt/debian-x86_64/opt/sysroot
 umount System/Kernel/Status/
 umount System/Kernel/Hardware/
 umount dev/
 
-rm -rf /opt/debian-aarch64/opt/sysroot/Users/root/system
-rm -rf /opt/debian-aarch64/opt/sysroot/Programs/blazeos/cache/*
+rm -rf /opt/debian-x86_64/opt/sysroot/Users/root/system
+rm -rf /opt/debian-x86_64/opt/sysroot/Programs/blazeos/cache/*
 
 #pack the sysroot for distribution
